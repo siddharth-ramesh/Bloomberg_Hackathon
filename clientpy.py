74f160830ba50ae417261bc4259162d0a685a68a
judearavinda <judecanada911@hotmail.com>
2015-07-25 00:27:34 -0400

    second commit

---
+++ clientpy.py
@@ -0,0 +1,50 @@
+import socket
+import sys
+
+def run(user, password, *commands):
+    HOST, PORT = "codebb.cloudapp.net", 17429
+
+    data=user + " " + password + "\n" + "\n".join(commands) + "\nCLOSE_CONNECTION\n"
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        sock.connect((HOST, PORT))
+        sock.sendall(data)
+        sfile = sock.makefile()
+        rline = sfile.readline()
+        while rline:
+            print(rline.strip())
+            rline = sfile.readline()
+    finally:
+        sock.close()
+
+def subscribe(user, password):
+    HOST, PORT = "codebb.cloudapp.net", 17429
+
+    data=user + " " + password + "\nSUBSCRIBE\n"
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        sock.connect((HOST, PORT))
+        sock.sendall(data)
+        sfile = sock.makefile()
+        rline = sfile.readline()
+        while rline:
+            print(rline.strip())
+            rline = sfile.readline()
+    finally:
+        sock.close()
+
+#runs any input, this is a placeholder for quick functions
+def runthis(input):
+   return run("Here_for_Beer","johnsmith",input)
+
+#BID <ticker> <price> <shares>
+def buy(ticker,price,shares):
+   return run("Here_for_Beer","johnsmith","BID "+str(ticker)+" "+ str(price)+" "+str(shares))
+
+#ASK <ticker> <price> <shares>
+def sell(ticker,price,shares):
+   return run("Here_for_Beer","johnsmith","ASK "+str(ticker)+" "+ str(price)+" "+str(shares))
