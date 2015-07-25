ead9f297b1de56d8528fb45adfd3eca911706d12
siddharth-ramesh <siddharth.ramesh@hotmail.com>
2015-07-25 01:49:47 -0400

    stock file

---
+++ Stock.py
@@ -0,0 +1,15 @@
+class Stock:
+
+    earnings = 0.0
+    num_of_shares = 0
+    divident_ratio = 0.0
+    initial_price = 0.0
+    market_value_per_share = 0.0
+    volatility = 0.0
+    ticker = ""
+
+    buy = 0.0
+    sell = 0.0
+
+    def __init__(self):
+        self.__init__()
