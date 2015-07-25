17ada5d714db728f3baf08245790749f9709421c
judearavinda <judecanada911@hotmail.com>
2015-07-25 03:18:52 -0400

    second commit

--- clientpy.py
+++ clientpy.py
@@ -2,10 +2,11 @@ import socket
 import sys
 
 def run(user, password, *commands):
+    global line
     HOST, PORT = "codebb.cloudapp.net", 17429
 
     data=user + " " + password + "\n" + "\n".join(commands) + "\nCLOSE_CONNECTION\n"
-
+    lines = []
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -14,10 +15,14 @@ def run(user, password, *commands):
         sfile = sock.makefile()
         rline = sfile.readline()
         while rline:
-            print(rline.strip())
+            line = rline.strip()
+            lines.append(line)
+            print(line)
             rline = sfile.readline()
     finally:
         sock.close()
+
+    return lines
 
 def subscribe(user, password):
     HOST, PORT = "codebb.cloudapp.net", 17429
@@ -90,3 +95,5 @@ def close_connection():
 def start_parsing():
     allSecurities = str( securities() )
 
+lines = securities()
+print(lines)
